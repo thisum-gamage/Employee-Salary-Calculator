c75875bd58ce1c54cb566368bf7ab8b86d2b2ddb
Thisum Gamage <thisumgmg@gmail.com>
2026-04-14 17:44:24 +0530

    inputs

---
+++ Salary Calculator.py
@@ -0,0 +1,7 @@
+print("--Welcome To Employee Salary Calculator--")
+print("-----------------------------------------")
+
+employee_name = input("Enter Your Name : ")
+basic_salary = float(input("Enter Your Basic Salary : Rs. "))
+allowance = float(input("Enter Your Allowance : "))
+bonus = float(input("Enter Your Bonus : "))
